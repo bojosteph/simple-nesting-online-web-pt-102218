a1838f53bf32b8d2b3aa99a958f420519611da53
Roberto Samia <bojosteph@gmail.com>
2018-10-29 17:03:21 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -59,7 +59,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-     my_hash = programmer_hash[:dennis_ritchie][:languages.to_s]
+     my_hash = programmer_hash[:dennis_ritchie][:languages]
      my_hash
 end
 
