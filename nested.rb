f98f316e7c9c3f28c83d0de8433a6cad315c7d31
Roberto Samia <bojosteph@gmail.com>
2018-10-29 17:23:21 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -59,7 +59,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-     my_hash = programmer_hash[:dennis_ritchie][:languages]
+     my_hash = programmer_hash[:dennis_ritchie][:languages].first
      
 end
 
